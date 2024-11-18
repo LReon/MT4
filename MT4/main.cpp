8da3687da514da02127a70e0d0701fce8651b674
Rio-Fujiwara <k023g0030@g.neec.ac.jp>
2024-11-18 18:06:06 +0900

    完了

--- MT4/main.cpp
+++ MT4/main.cpp
@@ -8,178 +8,135 @@ struct Vector3 {
     float x, y, z;
 };
 
-struct Matrix4x4 {
-    float m[4][4];
+struct Quaternion {
+    float x, y, z, w;
 };
 
-float Dot(const Vector3& v1, const Vector3& v2) { 
-    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-}
-float Length(const Vector3& v) { 
-    return std::sqrt(Dot(v, v)); 
-}
+Quaternion IdentityQuaternion() {
+    Quaternion result = {};
+    result.x = 0.0f;
+    result.y = 0.0f;
+    result.z = 0.0f;
+    result.w = 1.0f;
 
-Vector3 Cross(const Vector3& v1, const Vector3& v2) {
-    return { v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x };
-}
-
-
-Vector3 Normalize(const Vector3& v) {
-    float length = Length(v);
-    assert(length != 0.0f);
-    return { v.x / length, v.y / length, v.z / length };
+    return result;
 }
 
-Matrix4x4 DirectionToDirection(const Vector3& from, const Vector3& to) {
-    //from=u
-    //to=v
+Quaternion Multiply(const Quaternion& lhs, const Quaternion& rhs) {
+    Quaternion result = {};
+    result.x = (lhs.y * rhs.z) - (lhs.z * rhs.y) + (rhs.w * lhs.x) + (lhs.w * rhs.x);
+    result.y = (lhs.z * rhs.x) - (lhs.x * rhs.z) + (rhs.w * lhs.y) + (lhs.w * rhs.y);
+    result.z = (lhs.x * rhs.y) - (lhs.y * rhs.x) + (rhs.w * lhs.z) + (lhs.w * rhs.z);
+    result.w = (lhs.w * rhs.w) - (lhs.x * rhs.x) - (lhs.y * rhs.y) - (lhs.z * rhs.z);
 
-    //from,toのクロス積を取る
-    Vector3 cross = Cross(from,to);
-
-        //from,toの内積を取る
-    float cos = Dot(from, to);
-
-        //from,toの長さを取る
-    float sin = Length(cross);
-
-    float epsilon = 1e-6f;
-    Vector3 axis = {};
-    if (std::abs(cos + 1.0f) <= epsilon) {
-        if (std::abs(from.x) > epsilon || std::abs(from.y) > epsilon) {
-            //(ux≠0||uy≠0)の際のaxisの値を入れる
-            axis.x = from.y;
-            axis.y = -from.x;
-            axis.z = 0.0f;
-        }
-        else if (std::abs(from.x) > epsilon || std::abs(from.z) > epsilon) {
-            //(ux≠0||uz≠0)の際のaxisの値を入れる
-            axis.x = from.z;
-            axis.y = 0.0f;
-            axis.z = -from.x;
-        }
-        else {
-            // zero vector
-            assert(false);
-        }
-
-        axis = Normalize(axis);
-    }
-    else {
-        axis = Normalize(cross);
-    }
-
-    float oneMinusCos = (1.0f - cos);
-
-    Vector3 normalizedAxis = Normalize(axis);
-
-    float x = normalizedAxis.x;
-    float y = normalizedAxis.y;
-    float z = normalizedAxis.z;
-
-    //確認課題01_01同様中身を埋める
-    Matrix4x4 rotateMatrix = {};
-    rotateMatrix.m[0][0] = cos + x * x * oneMinusCos;
-    rotateMatrix.m[0][1] = x * y * oneMinusCos + z * sin;
-    rotateMatrix.m[0][2] = x * z * oneMinusCos - y * sin;
-    rotateMatrix.m[0][3] = 0.0f;
-
-    rotateMatrix.m[1][0] = y * x * oneMinusCos - z * sin;
-    rotateMatrix.m[1][1] = cos + y * y * oneMinusCos;
-    rotateMatrix.m[1][2] = y * z * oneMinusCos + x * sin;
-    rotateMatrix.m[1][3] = 0.0f;
-
-    rotateMatrix.m[2][0] = z * x * oneMinusCos + y * sin;
-    rotateMatrix.m[2][1] = z * y * oneMinusCos - x * sin;
-    rotateMatrix.m[2][2] = cos + z * z * oneMinusCos;
-    rotateMatrix.m[2][3] = 0.0f;
-
-    rotateMatrix.m[3][0] = 0.0f;
-    rotateMatrix.m[3][1] = 0.0f;
-    rotateMatrix.m[3][2] = 0.0f;
-    rotateMatrix.m[3][3] = 1.0f;
-
-        return rotateMatrix;
+    return result;
 }
 
 
+Quaternion Conjugate(const Quaternion& quaternion) {
+    Quaternion result = { - quaternion.x ,- quaternion.y, - quaternion.z,quaternion.w };
+    return result;
+}
 
+float Norm(const Quaternion& quaternion) {
+    return std::sqrtf((quaternion.x * quaternion.x) + (quaternion.y * quaternion.y) + (quaternion.z * quaternion.z) + (quaternion.w * quaternion.w));
+}
+
+Quaternion Normalize(const Quaternion& quaternion) {
+    
+    float norm = Norm(quaternion);
+
+    return { quaternion.x / norm,quaternion.y / norm,quaternion.z / norm,quaternion.w / norm };
+}
+
+Quaternion Inverse(const Quaternion& quaternion) {
+    //p12を参考にする。上記のNormalizeのlengthに該当する値をlengthの二乗に、クオータニオンに該当していた値をクオータニオンの共役に変えるだけ。
+    Quaternion conjugate = Conjugate(quaternion);
+    float norm = Norm(quaternion);
+	norm *= norm;
+    return { conjugate.x / norm,conjugate.y / norm,conjugate.z / norm,conjugate.w / norm };
+}
 
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
 
-void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
-    Novice::ScreenPrintf(x, y, "%s", label);
-    for (int row = 0; row < 4; ++row) {
-        for (int column = 0; column < 4; ++column) {
-            Novice::ScreenPrintf(
-                x + column * kColumnWidth, y + (row + 1) * kRowHeight, "%6.03f",
-                matrix.m[row][column]);
-        }
-    }
+void QuaternionScreenPrintf(int x, int y, Quaternion quaternion, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", quaternion.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", quaternion.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", quaternion.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%.02f", quaternion.w);
+	Novice::ScreenPrintf(x + kColumnWidth * 4, y, label);
 }
-
-
-// Windowsアプリでのエントリーポイント(main関数) 
+// Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-    // ライブラリの初期化 
-    const int kWindowWidth = 1280;
-    const int kWindowHeight = 720;
-    Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+	// ライブラリの初期化
+	const int kWindowWidth = 1280;
+	const int kWindowHeight = 720;
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
-    // キー入力結果を受け取る箱 
-    char keys[256] = { 0 };
-    char preKeys[256] = { 0 };
+	// キー入力結果を受け取る箱
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
-    // ウィンドウの×ボタンが押されるまでループ 
-    while (Novice::ProcessMessage() == 0) {
-        // フレームの開始 
-        Novice::BeginFrame();
+	Vector3 v1{ 1.0f, 3.0f, -5.0f };
+	Vector3 v2{ 4.0f, -1.0f, 2.0f };
+	
 
-        // キー入力を受け取る 
-        memcpy(preKeys, keys, 256);
-        Novice::GetHitKeyStateAll(keys);
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
 
-        /// 
-        /// ↓更新処理ここから 
-        /// 
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
 
-        Vector3 from0 = Normalize(Vector3{ 1.0f, 0.7f, 0.5f });
-        Vector3 to0 = { -from0.x,-from0.y, -from0.z };
-        Vector3 from1 = Normalize(Vector3{ -0.6f, 0.9f, 0.2f });
-        Vector3 to1 = Normalize(Vector3{ 0.4f, 0.7f, -0.5f });
-        Matrix4x4 rotateMatrix0 = DirectionToDirection(
-            Normalize(Vector3{ 1.0f, 0.0f, 0.0f }), Normalize(Vector3{ -1.0f, 0.0f, 0.0f }));
-        Matrix4x4 rotateMatrix1 = DirectionToDirection(from0, to0);
-        Matrix4x4 rotateMatrix2 = DirectionToDirection(from1, to1);
+		///
+		/// ↓更新処理ここから
+		///
 
-        /// 
-        /// ↑更新処理ここまで 
-        /// 
 
-        /// 
-        /// ↓描画処理ここから 
-        /// 
+		///
+		/// ↑更新処理ここまで
+		///
 
-        MatrixScreenPrintf(0, 0, rotateMatrix0, "rotateMatrix0");
-        MatrixScreenPrintf(0, kRowHeight * 5, rotateMatrix1, "rotateMatrix1");
-        MatrixScreenPrintf(0, kRowHeight * 10, rotateMatrix2, "rotateMatrix2");
+		///
+		/// ↓描画処理ここから
+		///
 
-        /// 
-        /// ↑描画処理ここまで 
-        /// 
+		Quaternion q1 = { 2.0f, 3.0f, 4.0f, 1.0f };
+		Quaternion q2 = { 1.0f, 3.0f, 5.0f, 2.0f };
+		Quaternion identity = IdentityQuaternion();
+		Quaternion conj = Conjugate(q1);
+		Quaternion inv = Inverse(q1);
+		Quaternion normal = Normalize(q1);
+		Quaternion mul1 = Multiply(q1, q2);
+		Quaternion mul2 = Multiply(q2, q1);
+		float norm = Norm(q1);
 
-        // フレームの終了 
-        Novice::EndFrame();
+		QuaternionScreenPrintf(0, kRowHeight * 0, identity, "   : Identity");
+		QuaternionScreenPrintf(0, kRowHeight * 1, conj, "   : Conjugate");
+		QuaternionScreenPrintf(0, kRowHeight * 2, inv, "   : Inverse");
+		QuaternionScreenPrintf(0, kRowHeight * 3, normal, "   : Normalize");
+		QuaternionScreenPrintf(0, kRowHeight * 4, mul1, "   : Multiply(q1, q2)");
+		QuaternionScreenPrintf(0, kRowHeight * 5, mul2, "   : Multiply(q2, q1)");
+		Novice::ScreenPrintf(0, kRowHeight * 6, "%.02f                          : Norm", norm);
 
-        // ESCキーが押されたらループを抜ける 
-        if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-            break;
-        }
-    }
+		///
+		/// ↑描画処理ここまで
+		///
 
-    // ライブラリの終了 
-    Novice::Finalize();
-    return 0;
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
 }
